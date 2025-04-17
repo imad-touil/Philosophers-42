3305e56896fc3ee7caf1e09ed7fc576d616d758d
imad-touil <imadtouil.it@gmail.com>
2025-04-17 04:13:35 +0100

    philosophers

--- learning.c
+++ learning.c
@@ -6,7 +6,7 @@
 /*   By: imatouil <imatouil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/05 22:07:06 by imatouil          #+#    #+#             */
-/*   Updated: 2025/04/16 02:46:16 by imatouil         ###   ########.fr       */
+/*   Updated: 2025/04/17 04:13:06 by imatouil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,18 +17,19 @@
 void	*myturn(void *arg)
 {
 	(void)arg;
-	while (1337)
-	{
-		printf("My Turn!\n");
-		sleep(1);
-	}
+	// while (1337)
+	// {
+	// 	printf("My Turn!\n");
+	// 	sleep(1);
+	// }
 	return (NULL);
 }
 
 void	urturn()
 {
 	// (void)arg;
-	while (1337)
+	// while (1337)
+	for (int i = 0; i < 4; i++)
 	{
 		printf("Your Turn!\n");
 		sleep(2);
@@ -43,5 +44,7 @@ int	main(void)
 	
 	// myturn();
 	pthread_create(&thread, NULL, myturn, NULL);
+	pthread_join(thread, NULL);
 	urturn();
+	pthread_detach(thread);
 }
