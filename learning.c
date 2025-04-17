8271227b8fdcc2c1df10aa49c3864546a1afab3b
imad-touil <imadtouil.it@gmail.com>
2025-04-17 05:42:04 +0100

    philosophers

--- learning.c
+++ learning.c
@@ -1,50 +1,16 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   learning.c                                         :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: imatouil <imatouil@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/05 22:07:06 by imatouil          #+#    #+#             */
-/*   Updated: 2025/04/17 04:13:06 by imatouil         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-#include <stdio.h>
-#include <unistd.h>
 #include <pthread.h>
+#include <stdio.h>
 
-void	*myturn(void *arg)
-{
+void *print_hello(void *arg) {
 	(void)arg;
-	// while (1337)
-	// {
-	// 	printf("My Turn!\n");
-	// 	sleep(1);
-	// }
-	return (NULL);
+    printf("Hello from the thread!\n");
+    return NULL;
 }
 
-void	urturn()
-{
-	// (void)arg;
-	// while (1337)
-	for (int i = 0; i < 4; i++)
-	{
-		printf("Your Turn!\n");
-		sleep(2);
-	}
-	// return (NULL);
-}
-	
-
-int	main(void)
-{
-	pthread_t	thread;
-	
-	// myturn();
-	pthread_create(&thread, NULL, myturn, NULL);
-	pthread_join(thread, NULL);
-	urturn();
-	pthread_detach(thread);
+int main() {
+    pthread_t thread;
+    pthread_create(&thread, NULL, print_hello, NULL);
+    // pthread_join(thread, NULL);  // Wait for the thread to finish
+    printf("Thread has finished\n");
+    return 0;
 }
